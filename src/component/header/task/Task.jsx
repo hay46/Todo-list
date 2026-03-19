bf6566aaf6ee7bb70b85e692805351ed32c9a418
haymanot <haymanotebabu2@gmail.com>
2026-03-20 01:15:20 +0300

    the form button

--- src/component/header/task/Task.jsx
+++ src/component/header/task/Task.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import './Task.css'
+import { FaBars } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 function Task() {
   return (
     <div className='header'>
+        <div className='header-content'>
+         <FaBars size={30} color="black" />
+    
         <h1>Todo-list-App</h1>
+        <FaSearch size={30} color="black" />
+        </div>
+        <div className='enter-task'>
+        <input type="text" placeholder='Enter your task' />
+        <button className='add-btn'>Add-Task</button>
+
+        </div>
     </div>
   )
 }
